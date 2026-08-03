7a21d2b455ba9a886c4e2ed4fafe9a13c3fa2bf4
Ritika Kumari Gupta <259775213+ritikakumarigupta@users.noreply.github.com>
2026-08-03 21:27:54 +0530

    Add productExceptSelf function in Solution class
    Implement productExceptSelf method to calculate the product of array elements except self.

---
+++ 01-Arrays/0238-productExceptSelf.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,1);
+   for(int i=0;i<n;i++){
+    int product=1;
+    for(int j=0;j<n;j++){
+        if(i!=j){
+            product*=nums[j];
+        }
+    }
+    
+    ans[i]=product;
+   }
+    return ans;
+    }
+};
